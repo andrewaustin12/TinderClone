9e3493b0be14003d5dd07913ebec629db38a368c
andrewaustin12 <andrew.d.austin2@gmail.com>
2024-05-04 15:05:43 +0200

    added initial project file structure and MainTabView

---
+++ TinderClone/TinderClone/App/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by andrew austin on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
